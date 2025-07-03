9a13451b5ad7a72bf80a797a46cdf933341bb333
Sebastián Egaña <sebaegana@gmail.com>
2025-07-02 22:53:31 -0400

    Actualization

---
+++ llm_example.R
@@ -0,0 +1,22 @@
+library(ellmer)
+
+chat_ollama(
+  system_prompt = NULL,
+  base_url = "http://localhost:11434",
+  model = "llama2",
+  seed = NULL,
+  api_args = list(),
+  echo = NULL,
+  api_key = NULL
+)
+
+models_ollama(base_url = "http://localhost:11434")
+
+chat <- chat_ollama(
+  base_url = "http://localhost:11434",
+  model = "llama2"
+)
+
+chat$chat("tell me a joke")
+
+live_console(chat)
